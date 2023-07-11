07f1cf0f7da72af563fcf1e734dff44fd0b1a909
John Spooner <elsporko@gmail.com>
2023-07-11 19:41:07 -0400

    Added support for picklist API and cleaned up users API

--- users/views.py
+++ users/views.py
@@ -5,8 +5,12 @@ from rest_framework.response import Response
 from .models import GrinchUser
 from .serializers import GrinchUserSerializer
 from rest_framework import status
+from django.contrib.auth.models import User
 
-class GrinchUser(ListAPIView):
+class GrinchUser(ListCreateAPIView):
     queryset = GrinchUser.objects.exclude(username='admin')
     serializer_class = GrinchUserSerializer
-
+    
+    def post(self, request):
+        data = request.data.copy()
+        user.objects.create_user(data.username, data,email, data.password)
