6141f91bb779fb5a75e77c0e3b577de8a6f33f08
John Spooner <elsporko@gmail.com>
2024-02-21 22:00:55 -0500

    APIs for login, logout have been built and tested.
    API for logout does not seem to work
    
    API for create user is built, but not yet tested.

--- users/serializers.py
+++ users/serializers.py
@@ -1,13 +1,40 @@
 from rest_framework import serializers
-from .models import GrinchUser
+from django.contrib.auth import get_user_model, authenticate
+from django.core.exceptions import ValidationError
+
+GrinchUser = get_user_model()
 
 
-class GrinchUserSerializer(serializers.ModelSerializer):
-    name = serializers.SerializerMethodField()
-
-    def get_name(obj):
-        return f'{obj.first_name} {obj.last_name}'
+class GrinchUserRegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = GrinchUser
-        fields = ['username', 'name', 'route', 'email', 'is_active']
+        fields = '__all__'
+
+    def create(self, clean_data):
+        user_obj = GrinchUser.objects.create_user(username=clean_data['username'],
+                                                  password=clean_data['password'])
+        user_obj.save()
+        return user_obj
+
+
+class GrinchLoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def check_user(self, clean_data):
+        user = authenticate(username=clean_data['username'], password=clean_data['password'])
+        if not user:
+            raise ValidationError('User not found')
+        return user
+
+
+class GrinchUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GrinchUser
+        fields = '__all__'
+
+    name = serializers.SerializerMethodField()
+
+    def get_name(self, obj):
+        return f'{obj.first_name} {obj.last_name}'
